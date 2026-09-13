625a313ef6e36d92a4bf01324325302225c2c47b
MoeJiyun233 <91889128+jiyun233@users.noreply.github.com>
2026-09-13 21:08:26 +0800

    20260910

---
+++ B2173.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+void solve()
+{
+    int T = 1;
+    cin >> T;
+    while (T--)
+    {
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    solve();
+
+    return 0;
+}
